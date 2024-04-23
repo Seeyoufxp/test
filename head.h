5577cb48276aee9c953d51559b7d7663bde7e28d
Seeyoufxp <seeyoufxp@gmail.com>
2024-04-23 17:24:17 +0800

    six

--- head.h
+++ head.h
@@ -1,30 +1,5 @@
 // #include <iostream>
 // #include <string>
 
-// class AbstractCalculator
-// {
-// public:
-//     virtual int getResult();
-//     int m_Num1;
-//     int m_Num2;
-// };
-
-// class AddCalculator:public AbstractCalculator
-// {
-// public:
-//     int getResult();
-// };
-
-// class SubCalculator:public AbstractCalculator
-// {
-// public:
-//     int getResult();
-// };
-
-// class MulCalculator:public AbstractCalculator
-// {
-// public:
-//     int getResult();
-// };
 
 // void test01();
