fee44e3242e478909408eac2c6cff505e95435dd
Seeyoufxp <seeyoufxp@gmail.com>
2024-04-23 11:39:52 +0800

    first

--- test.cpp
+++ test.cpp
@@ -1,28 +1,25 @@
 #include <iostream>
+#include <string>
 
-class Cube {
-private:
-    int length;
-    int width;
-    int height;
+class Base
+{
 
-public:
-    // 构造函数，设置默认边长为3, 2, 1
-    Cube(int l = 3, int w = 2, int h = 1) : length(l), width(w), height(h) {}
-
-    // 计算立方体体积的成员函数
-    int Compute() {
-        return length * width * height;
-    }
 };
 
-int main() {
-    int l, w, h;
-    std::cout << "输入立方体的长宽高：" << std::endl;
-    std::cin >> l >> w >> h;
-    Cube A(l, w, h);
-    Cube B;
-    std::cout << A.Compute() << std::endl;
-    std::cout << B.Compute() << std::endl;
+class Derived:public Base
+{
+
+};
+
+void test01()
+{
+
+}
+
+int main()
+{
+    test01();
+
+    system("pause");
     return 0;
 }
